dd02655f951ef1e01a93bedd8994f596841e82ac
Nina Azis <70008189+nixnaxnuxx@users.noreply.github.com>
2026-08-24 23:02:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MIMORU_CONFIG = {
-  supabaseUrl: "https://spyghfrnjryxgbmxwpdq.supabase.co",
+  supabaseUrl: "https://spyghfrnjryxgbmxwpdq.supabase.co/rest/v1/",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNweWdoZnJuanJ5eGdibXh3cGRxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NzM3MzYxOSwiZXhwIjoyMTAyOTQ5NjE5fQ.1adChHYD2h68am4ALa0Iv1XcUBXWIZ-HIPa_ubm54KI",
   bucketName: "mimoru-photos",
   pageSize: 30
